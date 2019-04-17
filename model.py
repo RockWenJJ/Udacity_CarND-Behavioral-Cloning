8be04b944230e0f1340d565f3136b27bf410db7c
JunjieWen <867040003@qq.com>
2019-04-17 09:12:13 +0800

    modify writeup

--- model.py
+++ model.py
@@ -1,17 +1,15 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from keras.models import Model, Sequential
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.applications.inception_v3 import InceptionV3
 from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense, Activation, Flatten,Dropout, Input, Lambda, GlobalAveragePooling2D, Cropping2D, Conv2D
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import os
 import csv
 import cv2
 import sklearn
-from sklearn.model_selection import train_test_split
 import random
 
 def get_data(path):
@@ -53,62 +51,6 @@ def generator(samples, path, batch_size=32):
             X_train = np.array(images)
             y_train = np.array(angles)
             yield X_train, y_train
-
-class network:
-    def __init__(self, freeze_flag, weights_flag, input_size, dropout, model_name):
-        self.freeze_flag = freeze_flag
-        self.weights_flag = weights_flag
-        self.model = self.build_model(input_size, dropout, model_name)
-
-    def build_model(self, input_size, dropout, model_name):
-        if model_name.lower() == "inception_v3":
-            net = InceptionV3(weights = self.weights_flag, include_top = False,
-                                input_shape = (input_size, input_size, 3))
-        elif model_name.lower() == "resnet50":
-            net = ResNet50(weights = self.weights_flag, include_top = False,
-                             input_shape = (input_size, input_size, 3))
-        else:
-            print("%s is not availible."%model_name)
-            return
-
-        if self.freeze_flag == True:
-            for layer in net.layers:
-                layer.trainable = False
-            ### other possible freeze strategy
-            # if model_name.lower() == "inception_v3":
-            #     for layer in net.layers:
-            #         # not freeze top conv layers and bn layers
-            #         if not layer.name in ["conv2d_94", "conv2d_86"] and not "batch_normalization" in layer.name:
-            #             layer.trainable = False
-            # if model_name.lower() == "resnet50":
-            #     for layer in net.layers:
-            #         layer.trainable = False
-
-        # input placeholder
-        model_input = Input(shape = (160, 320, 3))
-        resized_input = Lambda(lambda image: tf.image.resize_images(
-            image, (input_size, input_size)))(model_input)
-
-        # add extra trainable layers up to those freezed layers
-        x = net(resized_input)
-        x = GlobalAveragePooling2D()(x)
-        x = Dense(512, activation="relu")(x)
-        x = Dropout(dropout)(x)
-        predictions = Dense(1, activation="tanh")(x)
-
-        model = Model(inputs = model_input, outputs=predictions)
-        model.summary()
-        model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy'])
-
-        return model
-
-    def train(self, save_path, labels):
-        checkpoint = ModelCheckpoint(filepath = save_path, monitor = "val_loss", save_best_only = True)
-        stopper = EarlyStopping(monitor = "val_acc", min_delta = 3e-4, patience = 5)
-        self.model.fit_generator(callbacks=[checkpoint, stopper])
-
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        scaled = scaler.fit_transform(labels)
 
 def pilotnet():
     image = Input(shape=(160, 320, 3))
@@ -159,11 +101,6 @@ def resnet50():
 
     return model
 
-
-
-def data_process():
-    pass
-
 if __name__ == "__main__":
     path = "/data/DeepLearning/github_projects/Udacity_Project/beta_simulator_linux/data_0"
     batch_size = 32
@@ -178,7 +115,18 @@ if __name__ == "__main__":
     model.compile(loss='mse', optimizer='adam', metrics = ["accuracy"])
     checkpoint = ModelCheckpoint(filepath=save_path, monitor="val_loss", save_best_only=True)
     stopper = EarlyStopping(monitor="val_loss", min_delta=2e-5, patience=20)
-    model.fit_generator(train_generator, steps_per_epoch = np.ceil(len(train_samples) / batch_size), validation_data = valid_generator,
+    history_object = model.fit_generator(train_generator, steps_per_epoch = np.ceil(len(train_samples) / batch_size), validation_data = valid_generator,
                         validation_steps = np.ceil(len(valid_samples) / batch_size),epochs = 100, verbose = 1, callbacks=[checkpoint, stopper])
 
-    model.save("pilotnet.h5")
+    model.save("model.h5")
+
+    print(history_object.history.keys())
+
+    ### plot the training and validation loss for each epoch
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
--- writeup_template.md
+++ writeup_template.md
@@ -1,30 +1,66 @@
-# **Behavioral Cloning** 
-
-## Writeup Template
-
-### You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
+## **Behavioral Cloning** 
 
 ---
 
-**Behavioral Cloning Project**
+### 1.Steps
 
-The goals / steps of this project are the following:
+The steps of this project are the following:
 * Use the simulator to collect data of good driving behavior
 * Build, a convolution neural network in Keras that predicts steering angles from images
 * Train and validate the model with a training and validation set
 * Test that the model successfully drives around track one without leaving the road
 * Summarize the results with a written report
 
+### 2.Model Description
 
-[//]: # (Image References)
+The model archetecture referenced the pilotNet of NVIDIA, which is shown below:
 
-[image1]: ./examples/placeholder.png "Model Visualization"
-[image2]: ./examples/placeholder.png "Grayscaling"
-[image3]: ./examples/placeholder_small.png "Recovery Image"
-[image4]: ./examples/placeholder_small.png "Recovery Image"
-[image5]: ./examples/placeholder_small.png "Recovery Image"
-[image6]: ./examples/placeholder_small.png "Normal Image"
-[image7]: ./examples/placeholder_small.png "Flipped Image"
+| Layer         		|     Description	        					| 
+|:---------------------:|:---------------------------------------------:| 
+| Input         		| 160x320x3 RGB image   						| 
+| Cropping2D     	    | cropping=((50, 20), (0, 0))	                |
+| Resize                | outputs 66x200x3 RGB image                    |
+| Normalization         | net = net/127.5 - 1.                          |
+| Convolution 5x5		| 2x2 stride, valid padding, outputs 31x98x24	|
+| RELU	      	        | 				                                |
+| Convolution 5x5	    | 2x2 stride, valid padding, outputs 14x47x36   |
+| RELU					|												|
+| Convolution 5x5	    | 2x2 stride, valid padding, outputs 5x22x48    |
+| RELU  	      	    |  			                                    |
+| Convolution 3x3	    | 1x1 stride, valid padding, outputs 3x20x64    |
+| RELU					|												|
+| Convolution 3x3	    | 1x1 stride, valid padding, outputs 1x18x64    |
+| RELU	      	        |              			                        |
+| Convolution 3x3	    | 1x1 stride, same padding, outputs 4x4x32      |
+| RELU	      	        |              			                        |
+| Fully connected		| outputs 1164 classes                          |
+| RELU	      	        |              			                        |
+| Dropout	      	    | Dropout 0.5       			                |
+| Fully connected       | outputs 100 classes                           |
+| RELU	      	        |              			                        |
+| Dropout	      	    | Dropout 0.5       			                |
+| Fully connected       | outputs 10 classes                            |
+| RELU	      	        |              			                        |
+| Dropout	      	    | Dropout 0.5       			                |
+| Fully connected       | outputs 1 classes                             |
+| Tanh	      	        |              			                        |
+
+### 3.Training
+
+In training process, Data augmentation is applied using `np.fliplr()`. Adam optimizer was chosen because of its faster convergence and reduced oscillation. Batch size is 32 and total epochs is 50. Earlystop is applied with `val_loss` as monitor in order to get the model that could be generalized to the validation data set.
+
+The loss curves for training and validation data set are shown as following:
+
+
+
+### 4.Results
+
+The model training and validation loss curve is shown below:
+
+
+The testing result is shown below:
+
+
 
 ## Rubric Points
 ### Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
